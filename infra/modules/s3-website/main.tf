7482963020d55bdff15b9a8ae84366be49f30e84
amiradagdoug <amira.dagdoug@docc.techstarter.de>
2023-11-21 11:54:47 +0100

    feat: added cicd  commit

--- infra/modules/s3-website/main.tf
+++ infra/modules/s3-website/main.tf
@@ -28,3 +28,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
