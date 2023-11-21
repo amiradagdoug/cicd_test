6004979760fa19c79594cbbf030e513edcff33d4
amiradagdoug <amira.dagdoug@docc.techstarter.de>
2023-11-21 22:24:23 +0100

    feat: added cicd  commit

--- infra/modules/s3-website/main.tf
+++ infra/modules/s3-website/main.tf
@@ -30,7 +30,19 @@ resource "aws_s3_bucket_public_access_block" "this" {
 }
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.id
-  policy = jsonencoe({
-
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          "${aws_s3_bucket.this.arn}/*",
+          "${aws_s3_bucket.this.arn}",
+        ]
+      },
+    ]
   })
 }
