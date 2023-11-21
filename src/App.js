5d77b2d0c73a22823115d9e86688fb5a4f2b2207
amiradagdoug <amira.dagdoug@docc.techstarter.de>
2023-11-21 11:17:22 +0100

    feat: added cicd  commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import './App.css';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Techstarter/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <h1>My Techstarter Webiste</h1>
+  );
+}
+
+export default App;
